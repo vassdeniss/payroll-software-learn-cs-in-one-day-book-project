26e02ea3e9f6efe8dab96356fa40b31ffc5df9dd
Denis Vasilev <vassdeniss@gmail.com>
2021-12-02 14:25:18 +0200

    Added Staff.cs class

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace payroll_software_learn_cs_in_one_day_book_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Staff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace payroll_software_learn_cs_in_one_day_book_project
+{
+    internal class Staff
+    {
+        public Staff(string name, decimal rate)
+        {
+            StaffName = name;
+            _hourlyRate = rate;
+        }
+
+        private decimal _hourlyRate;
+        private int _hoursWorked;
+
+        public string StaffName { get; private set; }
+        public decimal TotalPay { get; protected set; }
+        public decimal BasicPay { get; private set; }
+        public int HoursWorked
+        {
+            get { return _hoursWorked; }
+            set
+            {
+                if (value > 0) _hoursWorked = value;
+                else _hoursWorked = 0;
+            }
+        }
+
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+            TotalPay = BasicPay = _hoursWorked * _hourlyRate;
+        }
+
+        public override string ToString()
+        {
+            return $"Staff member {StaffName} works {_hoursWorked} on a hourly rate of {_hourlyRate} with a total pay of {TotalPay}";
+        }
+    }
+}
