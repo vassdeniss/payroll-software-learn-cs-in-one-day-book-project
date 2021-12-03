e1283a132d686f985dd73d031c4f6a29c32f211f
Denis Vasilev <vassdeniss@gmail.com>
2021-12-03 11:30:51 +0200

    Added Admin.cs and FileReader.cs classes

---
+++ Admin.cs
@@ -0,0 +1,29 @@
+﻿namespace payroll_software_learn_cs_in_one_day_book_project
+{
+    internal class Admin : Staff
+    {
+        public Admin(string name) : base(name, _adminHourlyRate)
+        {
+        }
+
+        private const decimal _overtimeRate = 15.5m;
+        private const decimal _adminHourlyRate = 30;
+
+        public decimal Overtime { get; private set; }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            if (HoursWorked > 160)
+            {
+                Overtime = _overtimeRate * (HoursWorked - 160);
+                TotalPay += Overtime;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"\nStaff Member: {StaffName}\nWorked Hours: {HoursWorked}\nHourly Rate: {_adminHourlyRate}\nTotal Pay: {TotalPay}";
+        }
+    }
+}
---
+++ FileReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace payroll_software_learn_cs_in_one_day_book_project
+{
+    internal class FileReader
+    {
+        List<Staff> _staffList = new List<Staff>();
+        string[] _result = new string[2];
+        string _fileName = "staff.txt";
+
+        public List<Staff> ReadFile()
+        {
+            if (File.Exists(_fileName))
+            {
+                using StreamReader sr = new StreamReader(_fileName);
+                while (!sr.EndOfStream)
+                {
+                    _result = sr.ReadLine().Split(", ");
+
+                    if (_result[1] == "Manager") _staffList.Add(new Manager(_result[0]));
+                    else if (_result[1] == "Admin") _staffList.Add(new Admin(_result[0]));
+
+                    sr.Close();
+                }
+            }
+            else Console.WriteLine("Error! File not found!");
+
+            return _staffList;
+        }
+    }
+}
